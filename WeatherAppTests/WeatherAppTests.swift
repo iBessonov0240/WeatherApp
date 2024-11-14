8f66c72ff07244f054d11518b4b0fe8a18d7df77
Ilia <ilya2491@gmail.com>
2024-11-14 10:41:48 +0700

    Initial commit.

---
+++ WeatherAppTests/WeatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppTests.swift
+//  WeatherAppTests
+//
+//  Created by i0240 on 14.11.2024.
+//
+
+import Testing
+@testable import WeatherApp
+
+struct WeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
